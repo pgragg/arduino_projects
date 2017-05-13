86fb5dfce15db476015145b78e885b52afe0e865
Piper Gragg <pipergragg@gmail.com>
2017-05-13 16:33:15 -0400

    Added open weather key and api call.

--- ESP8266_Shield_destServer/server/sinatra_server.rb
+++ ESP8266_Shield_destServer/server/sinatra_server.rb
@@ -1,4 +1,11 @@
 require 'sinatra'
+require 'httparty'
+
+require 'yaml'
+keys = YAML.load_file "keys.yml"
+api_key = keys["open_weather"]
+
 get '/' do
-  'Hello world!'
+	res = HTTParty.get("http://api.openweathermap.org/data/2.5/forecast?zip=94040,us&APPID=#{api_key}")
+  res
 end
